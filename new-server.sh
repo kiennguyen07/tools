1cdeab1e89a80380d83bffcd0f3eaa2baacc5380
Kien Nguyen <kiennguyen.hut@gmail.com>
2021-09-14 15:54:31 +0700

    Initial commit

---
+++ new-server.sh
@@ -0,0 +1,11 @@
+apt-get install nginx
+apt-get update
+apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+echo "{\"log-driver\": \"json-file\",\"log-opts\": {\"max-size\": \"256m\",\"max-file\": \"5\"}}"  >   /etc/docker/daemon.json
